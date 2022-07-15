9adf5d5081c11136b228cb28549978b79902d4e4
salimdawood <salimdawoodcareer@gmail.com>
2022-07-15 10:16:55 +0300

    social-media-validation

--- src/constantVariables/cardInfoInput.js
+++ src/constantVariables/cardInfoInput.js
@@ -4,15 +4,17 @@ export const cardInfoInput = [
     name:"title",
     type:"text",
     placeholder:"Enter your card title",
-    errorMessage:"Title should be between 3-30 characters, and should include only letters,numbers,',and spaces",
+    errorMessage:"Title should be between 3-30 characters and can only contain letters, numbers, ' , and spaces",
     required:true,
     label:"Title *",
-    pattern:"^[a-zA-Z0-9\u0621-\u064A\u0660-\u0669 ']{3,30}$"
+    pattern:"[a-zA-Z0-9\u0621-\u064A\u0660-\u0669 ']{3,30}"
   },
   {
     id:2,
     name:"facebookLink",
     type:"text",
+    pattern:"[a-zA-Z0-9\u0621-\u064A\u0660-\u0669.]{5,50}",
+    errorMessage:"Facebook name should be between 5-50 characters and can only contain letters, numbers, and periods.",
     placeholder:"Shop facebook page name",
     label:"Facebook link"
   },
@@ -20,6 +22,8 @@ export const cardInfoInput = [
     id:3,
     name:"instagramLink",
     type:"text",
+    pattern:"[a-zA-Z0-9\u0621-\u064A\u0660-\u0669_.]{1,30}",
+    errorMessage:"Instagram name should be between 1-30 characters and can only contain letters, numbers, period, and underscores.",
     placeholder:"Shop instagram page name",
     label:"Instagram link"
   },
