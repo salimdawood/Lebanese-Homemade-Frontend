d4369cfea37dc2a19b805a3d326b7822a8858f8d
salimdawood <salimdawoodcareer@gmail.com>
2022-07-16 12:13:45 +0300

    code-clean

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,6 +7,8 @@ import {URL_PATH} from '../constantVariables/path'
 import useAuth from '../hooks/useAuth'
 //context
 import {notificationContext} from '../context/notificationContext'
+//components
+import FormInput from '../components/FormInput'
 
 const SignIn = () => {
 
@@ -57,19 +59,43 @@ const SignIn = () => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   }
 
+  const userInput = [
+    {
+      id:1,
+      name:"name",
+      type:"text",
+      placeholder:"Enter your name",
+      errorMessage:"User name should be between 3-30 characters, and should include only letters,numbers,' and spaces.",
+      required:true,
+      label:"Username *",
+      pattern:"^[a-zA-Z0-9\u0621-\u064A\u0660-\u0669 ']{3,30}$"
+    },
+    {
+      id:2,
+      name:"password",
+      type:"password",
+      placeholder:"Enter your password",
+      errorMessage:"Password should be between 8-20 characters, and must include at least: one letter,one number, and one special character(!@#$%^&*).",
+      label:"Password *",
+      pattern:"(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}",
+      required:true
+    }, 
+  ]
+
   return (
     <form onSubmit={handleSubmit} className="sign-up-form">
       <h1>Sign in</h1>
       <div className="form-container">
         <span className="db-warning">{warningMessage}</span>
-        <div className="form-input">
-          <label>User name *</label>
-          <input type="text" placeholder="Enter your name" name = "name"  value={userInfo.name} onChange={handleChange} required/>
-        </div>
-        <div className="form-input">
-          <label>Password *</label>
-          <input type="password" placeholder="Enter your password" name = "password" value={userInfo.password} onChange={handleChange} required/>
-        </div>
+        {
+          userInput.map((input)=>(
+            <FormInput
+            key={input.id}
+            {...input}
+            value={userInfo[input.name]}
+            onChange={handleChange} />
+          ))
+        }
         <div className="form-checkbox">
           <input type="checkbox" value="remember-me" />
           <label>Remember me</label>
