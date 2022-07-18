65a38c8eaa1a08e44159ab4cbf03edfb4afa0256
salimdawood <salimdawoodcareer@gmail.com>
2022-07-18 22:14:28 +0300

    nav-show-active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useLocation } from 'react-router-dom'
+//component
 import {Hamburger,Close} from './Svg.jsx'
 
 const Navbar = () => {
-
   
   const [isOpen,setIsOpen] = useState(false)
+  const location = useLocation()
+
   return (
     <>
       <div className="nav-logo center">
@@ -13,10 +15,18 @@ const Navbar = () => {
       </div>
       <div className={isOpen?"nav-link":"nav-link nav-mobile"}>
         <ul>
-          <li><Link to="/signup">Sign up</Link></li>
-          <li><Link to="/contactus">Contact us</Link></li>
-          <li><Link to="/aboutus">About us</Link></li>
-          <li><Link to="/signin">Log in</Link></li>
+          <li>
+            <Link className={location.pathname.includes('/signup')?"active":""} to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link className={location.pathname.includes('/contactus')?"active":""} to="/contactus">Contact us</Link>
+          </li>
+          <li>
+            <Link className={location.pathname.includes('/aboutus')?"active":""} to="/aboutus">About us</Link>
+          </li>
+          <li>
+            <Link className={location.pathname.includes('signin')?"active":""} to="/signin">Log in</Link>
+          </li>
         </ul>
       </div>
       <div onClick={()=>setIsOpen(!isOpen)} className="nav-burger">
